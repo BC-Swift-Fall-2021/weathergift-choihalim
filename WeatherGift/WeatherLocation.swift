4a97472a4f42a1d8497d40f15b263805927413ba
Halim Choi <choizk@bc.edu>
2021-10-09 22:19:11 -0400

    WeatherGift with .gitignore

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by 최하림 on 10/9/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
