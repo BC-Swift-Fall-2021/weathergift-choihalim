3ea8be2065e3427bda6823bac95e5d710647335d
Halim Choi <choizk@bc.edu>
2021-10-09 20:53:24 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by 최하림 on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
