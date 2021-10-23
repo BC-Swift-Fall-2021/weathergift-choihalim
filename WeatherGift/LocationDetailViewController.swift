6d6119f0740aea814e9e29d1faf30c6b8a104b31
Halim Choi <choizk@bc.edu>
2021-10-22 21:08:35 -0400

    6.16 Getting Hourly Weather Data from the OpenWeather API

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 private let dateFormatter: DateFormatter = {
-    print("🗓 JUST CREATED A DATE FORMATTER")
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE, MMM d"
     return dateFormatter
